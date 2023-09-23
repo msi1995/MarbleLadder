f4ac4eead84992bf392bbd305965ba44987602a9
msi1995 <lloyddo@oregonstate.edu>
2023-09-23 04:24:19 -0700

    fix buggy state persistence from match report modal

--- client/src/components/SoloLadder.tsx
+++ client/src/components/SoloLadder.tsx
@@ -174,6 +174,10 @@ export const SoloLadder = () => {
   };
 
   const handleOpenMatchReportModal = async (event: any) => {
+    setReporterIsWinner(null);
+    setPlayerScore(null);
+    setOpponentScore(null);
+    setReportedMap("Select")
     setReportMatchModalVisible(true);
   };
 
@@ -469,8 +473,6 @@ export const SoloLadder = () => {
                 <button
                   onClick={() => {
                     setReportMatchModalVisible(false);
-                    setReporterIsWinner(null);
-                    setPlayerOpponent("");
                   }}
                   type="submit"
                   className="block w-full rounded-md bg-red-500 hover:bg-red-700 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
