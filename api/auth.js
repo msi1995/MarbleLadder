ee7334a283e59c269b6438eb6bbec83f3d8dda7a
msi1995 <lloyddo@oregonstate.edu>
2023-09-22 22:16:36 -0700

    unbreak auth

--- api/auth.js
+++ api/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = async (req, res, next) => {
   try {
     //   get the token from the authorization header
-    const token = await req.headers.authorization.split(" ")[10000];
+    const token = await req.headers.authorization.split(" ")[1];
 
     //check if the token matches the supposed origin
     const decodedToken = await jwt.verify(token, "RANDOM-TOKEN");
