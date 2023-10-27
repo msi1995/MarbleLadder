17bbb2273edfe68b295c752432ea5c72d429ca01
Doug L <63132397+msi1995@users.noreply.github.com>
2023-10-26 20:04:20 -0700

    fix css gore in contact and update some text

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -1,13 +1,10 @@
 export const Contact = () => {
   return (
-    <div className="flex pt-16 h-screen w-screen relative overflow-x-hidden justify-center items-center">
-      <div className="flex flex-col sm:w-2/5 w-4/5 relative overflow-x-hidden sm:px-16 py-8 overflow-y-hidden justify-start items-start text-left bg-black-opacity-90 rounded-md align-center">
-      <span className="block w-full sm:text-4xl text-2xl text-white pb-4 text-center">Contact</span>
-        <span className="block w-full sm:text-2xl text-xl px-4 py-4 text-white">
-          - Discord @ msi#8029
-        </span>
-        <span className="block w-full sm:text-2xl text-xl px-4 py-4 text-white">
-          - lloyd.dg7@gmail.com
+    <div className="flex pt-16 h-screen relative justify-center items-center">
+      <div className="sm:w-1/3 flex flex-col text-white items-center px-8 py-8 bg-black-opacity-90 rounded-md">
+      <span className="sm:text-4xl text-2xl pb-8">Contact</span>
+        <span className="sm:text-xl text-lg">
+          Discord @ msi#8029
         </span>
       </div>
     </div>
