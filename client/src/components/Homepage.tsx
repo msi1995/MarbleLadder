445fb82251fb4ef29b6bd6affcc84daeda12828c
Doug <63132397+msi1995@users.noreply.github.com>
2023-11-12 01:37:06 -0800

    try force overflow y hidden app wide (#70)

--- client/src/components/Homepage.tsx
+++ client/src/components/Homepage.tsx
@@ -10,16 +10,15 @@ export const Homepage = () => {
             ? "Welcome to MarbleLadder!"
             : `Welcome to MarbleLadder, ${username}!`}
         </div>
-        <div className="select-none text-center neon-text-allsize sm:text-3xl text-xl text-white">
+        <div className="select-none text-center neon-text sm:text-3xl text-xl text-white">
           <a href='https://discord.gg/marbleitup'>Click to join the MIU Discord and find opponents!</a>
         </div>
       </div>
-      <div className="fixed bottom-0 py-4 w-full bg-black/70 items-center justify-center">
-        <span className="block w-full border-0 px-4 border-solid border-red-600 sm:text-4xl text-lg text-white text-center">
+      <div className="fixed bottom-0 py-4 w-full bg-black/70 items-center justify-center text-white text-center">
+        <span className="block w-full sm:px-4 sm:pb-8 sm:text-4xl text-lg">
           MarbleLadder implements features supporting <i className='neon-text-allsize'>Marble It Up! Ultra</i> ranked matches and gem hunt record tracking!
         </span>
-        <br />
-        <span className="block w-full sm:px-56 px-4 mt-4 border-0 border-solid border-red-600 sm:text-xl text-sm text-white text-center">
+        <span className="block w-full sm:px-64 px-4 mt-4 sm:text-xl text-sm">
           Play and report online 1v1 matches against opponents on PC, Xbox, PlayStation or Switch to try and climb the 1v1 Gem Hunt ladder, or play solo to try your luck at setting a Solo Gem Hunt world record! You can look for opponents in the
           MIU Discord and then host a private game to play ranked ladder matches.
         </span>
