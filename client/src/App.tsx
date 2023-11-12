f5bd906de5d864f7ef4fc9bbad8235433f92c584
Doug L <63132397+msi1995@users.noreply.github.com>
2023-11-12 02:29:03 -0800

    restore navbar, set viewport settings to try and resolve chrome bug

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <BrowserRouter>
-    {/* <Navbar/> */}
+    <Navbar/>
     <div className="fixed -z-10 w-screen h-screen bg-cover bg-fixed bg-[url('/public/MIU_1.jpg')]" />
     <LadderData.Provider value={ladderData}>
       <Routes>
