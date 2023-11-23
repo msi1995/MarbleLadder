4982284212784553ce917da96a8fb06f853acc3c
Doug <63132397+msi1995@users.noreply.github.com>
2023-11-23 00:27:25 -0800

    update site background image (#104)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <BrowserRouter>
     <Navbar/>
-    <div className="fixed -z-10 w-screen h-screen bg-cover bg-fixed bg-[url('/public/MIU_1.webp')]" />
+    <div className="fixed -z-10 w-screen h-screen bg-cover bg-fixed brightness-75 bg-[url('/public/miu_2.avif')]" />
     <LadderData.Provider value={ladderData}>
       <Routes>
         <Route path="/" element={<Homepage/>} />
