f58281f5ef40105ea7d5a3db3c8f82d2d1d011fe
Doug <63132397+msi1995@users.noreply.github.com>
2023-11-27 20:51:37 -0800

    Remove extra navbar divider (#119)

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -199,22 +199,6 @@ export const Navbar = () => {
               About
             </NavLink>
           </li>
-          <li className="text-pink-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              className="w-4 h-4 current-fill"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-              />
-            </svg>
-          </li>
         </ul>
 
         {token ? (
